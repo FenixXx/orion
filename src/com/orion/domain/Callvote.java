997518c8c434b66abe8fe2542909e1c367ab5d88
mathias <mvmalderen19@gmail.com>
2013-05-31 17:31:58 +0200

    Apply Builder Pattern (EJ, Item 2) to Callvote class.

--- src/com/orion/domain/Callvote.java
+++ src/com/orion/domain/Callvote.java
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  * 
  * @author      Daniele Pantaleone
- * @version     1.0
+ * @version     1.1
  * @copyright   Daniele Pantaleone, 08 October, 2012
  * @package     co.orion.domain
  **/
@@ -29,66 +29,293 @@ import org.joda.time.DateTime;
 
 public class Callvote {
     
-    public int id;
-    public Client client;
-    public String type;
-    public String data;
-    public int yes;
-    public int no;
-    public DateTime time_add;
-    public DateTime time_edit;
+    private int id;
+    private Client client;
+    private String type;
+    private String data;
+    private int yes;
+    private int no;
+    private DateTime time_add;
+    private DateTime time_edit;
     
     
-    
-    /**
-     * Object constructor
-     * 
-     * @author Daniele Pantaleone
-     * @param  client The <tt>Client</tt> this <tt>Callvote</tt> belongs to
-     * @param  type The <tt>Callvote</tt> type
-     * @param  data The <tt>Callvote</tt> additional data
-     **/
-    public Callvote(Client client, String type, String data) {
-    	this.client = client;
-    	this.type = type;
-    	this.data = data;
-    	this.yes = 1;
+    private Callvote(Builder builder) {
+    	this.setId(builder.id);
+    	this.setClient(builder.client);
+    	this.setType(builder.type);
+    	this.setData(builder.data);
+    	this.setYes(builder.yes);
+    	this.setNo(builder.no);
+    	this.setTimeAdd(builder.time_add);
+    	this.setTimeEdit(builder.time_edit);
     }
     
     
     /**
-     * Object constructor
+     * Set the callvote time add.
      * 
-     * @author Daniele Pantaleone
-     * @param  id The <tt>Callvote</tt> id
-     * @param  client The <tt>Client</tt> this <tt>Callvote</tt> belongs to
-     * @param  type The <tt>Callvote</tt> type
-     * @param  data The <tt>Callvote</tt> additional data
-     * @param  yes The amount of yes of this <tt>Callvote</tt>
-     * @param  no The amount of no of this <tt>Callvote</tt>
-     * @param  time_add The <tt>DateTime</tt> when this <tt>Callvote</tt> has been created
-     * @param  time_edit The <tt>DateTime</tt> when this <tt>Callvote</tt> has been last edited
+     * @author Mathias Van Malderen
+     * @param  time_add A <tt>DateTime</tt> object representing the time when the callvote has been raised
      **/
-    public Callvote(int id, Client client, String type, String data, int yes, int no, DateTime time_add, DateTime time_edit) {
-    	this.id = id;
-    	this.client = client;
-    	this.type = type;
-    	this.data = data;
-    	this.yes = yes;
-    	this.no = no;
-    	this.time_add = time_add;
-    	this.time_edit = time_edit;
+    public void setTimeAdd(DateTime time_add) {
+        this.time_add = time_add;
+    }
+
+    
+    /**
+     * Set the callvote last time edit.
+     * 
+     * @author Mathias Van Malderen
+     * @param  time_edit A <tt>DateTime</tt> object representing the callvote last time edit
+     **/
+    public void setTimeEdit(DateTime time_edit) {
+        this.time_edit = time_edit;
     }
     
     
     /**
+     * Return the callvote id.
+     * 
+     * @author Mathias Van Malderen
+     * @return The callvote id
+     **/
+    public int getId() {
+		return id;
+	}
+
+
+    /**
+     * Set the callvote id.
+     * 
+     * @author Mathias Van Malderen
+     * @return The callvote id
+     **/
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	
+	/**
+	 * Get client who raised the callvote.
+	 * 
+	 * @author Mathias Van Malderen
+	 * @return The client who raised this callvote.
+	 */
+	public Client getClient() {
+		return client;
+	}
+
+	
+	/**
+	 * Set client who raised the callvote.
+	 * 
+	 * @author Mathias Van Malderen
+	 * @return The client who raised this callvote.
+	 */
+	public void setClient(Client client) {
+		
+		if (client == null)
+			throw new NullPointerException("client");
+		
+		this.client = client;
+	}
+	
+
+	/**
+	 * Get the type of this <tt>Callvote</tt>
+	 * 
+	 * @author Mathias Van Malderen
+	 * @return The type of this <tt>Callvote</tt>
+	 */
+	public String getType() {
+		return this.type;
+	}
+
+	
+	/**
+	 * Set client who raised the callvote.
+	 * 
+	 * @author Mathias Van Malderen
+	 * @return The client who raised this callvote.
+	 */
+	public void setType(String type) {
+		
+		if (type == null)
+			throw new NullPointerException("type");
+		
+		this.type = type;
+		
+	}
+
+	
+	/**
+	 * Get optional data associated with the callvote.
+	 * 
+	 * @author Mathias Van Malderen
+	 * @return parameter value of the callvote if one was specified, null otherwise.
+	 */
+	public String getData() {
+		return this.data;
+	}
+
+
+	/**
+	 * Set optional data associated with the callvote.
+	 * 
+	 * @author Mathias Van Malderen
+	 * @param data parameter value of the callvote
+	 */
+	public void setData(String data) {
+		this.data = data;
+	}
+
+	
+	/**
+	 * Get the amount of yes votes for this <tt>Callvote</tt>
+	 * 
+	 * @author Mathias Van Malderen
+	 * @param yes number of 'yes' votes
+	 */
+	public int getYes() {
+		return this.yes;
+	}
+
+
+	/**
+	 * Set the amount of yes answers for this <tt>Callvote</tt>
+	 * 
+	 * @author Mathias Van Malderen
+	 * @param yes number of 'yes' votes
+	 */
+	public void setYes(int yes) {
+		
+		if (yes < 1)
+			throw new IllegalArgumentException("number of 'yes' answers cannot be lower than 1");
+		
+		this.yes = yes;
+		
+	}
+
+
+	/**
+	 * Get the amount of no votes for this <tt>Callvote</tt>
+	 * 
+	 * @author Mathias Van Malderen
+	 * @param no number of 'no' votes
+	 */
+	public int getNo() {
+		return this.no;
+	}
+	
+	
+	/**
+	 * Set the amount of no answers for this <tt>Callvote</tt>
+	 * 
+	 * @author Mathias Van Malderen
+	 * @param no number of 'no' votes
+	 */
+	public void setNo(int no) {
+		
+		if (no < 0)
+			throw new IllegalArgumentException("number of 'no' answers cannot be negative");
+		
+		this.no = no;
+		
+	}
+	
+	
+    /**
+     * Return a <tt>DateTime</tt> object representing the time when the callvote has been raised
+     * 
+     * @author Mathias Van Malderen
+     * @return A <tt>DateTime</tt> object representing the time when the client has been raised
+     **/
+    public DateTime getTimeAdd() {
+        return this.time_add;
+    }
+    
+    
+    /**
+     * Return a <tt>DateTime</tt> object representing the callvote last time edit
+     * 
+     * @author Mathias Van Malderen
+     * @return A <tt>DateTime</tt> object representing the callvote last time edit
+     **/
+    public DateTime getTimeEdit() {
+        return this.time_edit;
+    }
+
+
+	/**
      * String object representation
      * 
      * @author Daniele Pantaleone
      * @return A <tt>String</tt> representing the content of this object
      **/
     public String toString() {
-        return "[ id : " + this.id + " | client : " + this.client.id + " | type : " + this.type + " | data : " + this.data + " | yes : " + this.yes + " | no : " + this.no + " | time_add : " + this.time_add.toString() + " | time_add : " + this.time_add.toString() + " ]";
+        return "[ id : " + this.id + " | client : " + this.client.getId() + " | type : " + this.type + " | data : " + this.data + " | yes : " + this.yes + " | no : " + this.no + " | time_add : " + this.time_add.toString() + " | time_add : " + this.time_add.toString() + " ]";
+    }
+    
+    
+    public static class Builder {
+    	
+    	private final Client client;
+    	private final String type;
+    	
+        private int id;
+        private String data;
+        private int yes;
+        private int no;
+        private DateTime time_add;
+        private DateTime time_edit;
+    	
+        
+    	public Builder(Client client, String type) {
+    		this.client = client;
+    		this.type = type;
+    	}
+    	
+    	
+    	public Builder data(String value) {
+    		this.data = value;
+    		return this;
+    	}
+    	
+    	
+    	public Builder yes(int value) {
+    		this.yes = value;
+    		return this;
+    	}
+    	
+    	
+    	public Builder no(int value) {
+    		this.yes = value;
+    		return this;
+    	}
+    	
+    	
+    	public Builder id(int value) {
+    		this.id = value;
+    		return this;
+    	}
+    	
+    	
+    	public Builder timeAdd(DateTime value) {
+    		this.time_add = value;
+    		return this;
+    	}
+    	
+    	
+    	public Builder timeEdit(DateTime value) {
+    		this.time_edit = value;
+    		return this;
+    	}
+    	
+    	
+    	public Callvote build() {
+    		return new Callvote(this);
+    	}
+    	
     }
     
 }
