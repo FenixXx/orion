a5985ced6ede90bc6f18f17c5c60b5442109e4df
Fenix <fenix@urbanterror.info>
2013-05-31 15:18:36 +0200

    Refactored Group class: added set/get methods + adjusted constructor

--- src/com/orion/domain/Group.java
+++ src/com/orion/domain/Group.java
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  * 
  * @author      Daniele Pantaleone
- * @version     1.0
+ * @version     1.1
  * @copyright   Daniele Pantaleone, 05 October, 2012
  * @package     com.orion.domain
  **/
@@ -27,10 +27,10 @@ package com.orion.domain;
 
 public class Group {
     
-    public int      id;
-    public String   name;
-    public String   keyword;
-    public int      level;
+    private int id;
+    private String name;
+    private String keyword;
+    private int level;
   
     
     /**
@@ -43,12 +43,98 @@ public class Group {
      * @param  level The group level
      **/
     public Group(int id, String name, String keyword, int level) {
-        
-        this.id = id;
-        this.name = name;
-        this.keyword = keyword;
-        this.level = level;
+        this.setId(id);
+        this.setName(name);
+        this.setKeyword(keyword);
+        this.setLevel(level);
+    }
+
     
+    /**
+     * Return the group id
+     * 
+     * @author Daniele Pantaleone
+     * @return The group id
+     **/
+    public int getId() {
+        return this.id;
+    }
+    
+    
+    /**
+     * Return the group name
+     * 
+     * @author Daniele Pantaleone
+     * @return The group name
+     **/
+    public String getName() {
+        return this.name;
+    }
+    
+    
+    /**
+     * Return the group keyword
+     * 
+     * @author Daniele Pantaleone
+     * @return The group keyword
+     **/
+    public String getKeyword() {
+        return this.keyword;
+    }
+    
+    
+    /**
+     * Return the group level
+     * 
+     * @author Daniele Pantaleone
+     * @return The group level
+     **/
+    public int getLevel() {
+        return this.level;
+    }
+    
+    
+    /**
+     * Set the group id
+     * 
+     * @author Daniele Pantaleone
+     * @param  id The group id
+     **/
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    
+    /**
+     * Set the group name
+     * 
+     * @author Daniele Pantaleone
+     * @param  name The group name
+     **/
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    
+    /**
+     * Set the group keyword
+     * 
+     * @author Daniele Pantaleone
+     * @param  keyword The group keyword
+     **/
+    public void setKeyword(String keyword) {
+        this.keyword = keyword;
+    }
+
+    
+    /**
+     * Set the group level
+     * 
+     * @author Daniele Pantaleone
+     * @param  level The group level
+     **/
+    public void setLevel(int level) {
+        this.level = level;
     }
     
     
@@ -59,7 +145,7 @@ public class Group {
      * @return A <tt>String</tt> representing this object
      **/
     public String toString() {
-        return "[ id : " + this.id + " | name : " + this.name + " | keyword : " + this.keyword + " | level : " + this.level + " ]";
+        return "[ id : " + this.getId() + " | name : " + this.getName() + " | keyword : " + this.getKeyword() + " | level : " + this.getLevel() + " ]";
     }
     
 }
