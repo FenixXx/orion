21cd42ab4fcc8ddfe8e0182c5d1dc183b5a2479a
mathias <mvmalderen19@gmail.com>
2013-05-31 23:48:39 +0200

    Replace TABs by SPACEs in Callvote

--- src/com/orion/domain/Callvote.java
+++ src/com/orion/domain/Callvote.java
@@ -262,10 +262,10 @@ public class Callvote {
         
         
         public Builder(Client client, String type) {
-        	
-        	if (client == null) throw new NullPointerException("client cannot be null");
-        	if (type == null) throw new NullPointerException("type cannot be null");
-        	
+            
+            if (client == null) throw new NullPointerException("client cannot be null");
+            if (type == null) throw new NullPointerException("type cannot be null");
+            
             this.client = client;
             this.type = type;
             
