c622a8351d8b46f1bbaa03b72805288187212b52
Fenix <fenix@urbanterror.info>
2013-05-31 15:07:53 +0200

    Refactored Alias class: added set/get methods + adjusted constructors

--- src/com/orion/domain/Alias.java
+++ src/com/orion/domain/Alias.java
@@ -18,7 +18,7 @@
  * Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
  * 
  * @author      Mathias Van Malderen, Daniele Pantaleone
- * @version     1.0
+ * @version     1.1
  * @copyright   Mathias Van Malderen, 05 October, 2012
  * @package     com.orion.domain
  **/
@@ -29,12 +29,12 @@ import org.joda.time.DateTime;
 
 public class Alias {
     
-    public int id;
-    public Client client;
-    public String name;
-    public int num_used;
-    public DateTime time_add;
-    public DateTime time_edit;
+    private int id;
+    private Client client;
+    private String name;
+    private int num_used;
+    private DateTime time_add;
+    private DateTime time_edit;
     
     
     /**
@@ -45,9 +45,9 @@ public class Alias {
      * @param  name The <tt>Alias</tt> name
      **/
     public Alias(Client client, String name) {
-        this.client = client;
-        this.name = name;
-        this.num_used = 1;
+        this.setClient(client);
+        this.setName(name);
+        this.setNumUsed(this.getNumUsed() + 1);
     }
     
     
@@ -63,11 +63,143 @@ public class Alias {
      * @param  time_edit The <tt>DateTime</tt> when this <tt>Alias</tt> has been last edited
      **/
     public Alias(int id, Client client, String name, int num_used, DateTime time_add, DateTime time_edit) {
+        this.setId(id);
+        this.setClient(client);
+        this.setName(name);
+        this.setNumUsed(num_used);
+        this.setTimeAdd(time_add);
+        this.setTimeEdit(time_edit);
+    }
+
+    
+    /**
+     * Return the alias id
+     * 
+     * @author Daniele Pantaleone
+     * @return The alias id
+     **/
+    public int getId() {
+        return this.id;
+    }
+    
+    
+    /**
+     * Return the <tt>Client</tt> who this alias belongs to
+     * 
+     * @author Daniele Pantaleone
+     * @return The <tt>Client</tt> who this alias belongs to
+     **/
+    public Client getClient() {
+        return this.client;
+    }
+    
+    
+    /**
+     * Return the alias name
+     * 
+     * @author Daniele Pantaleone
+     * @return The alias name
+     **/
+    public String getName() {
+        return this.name;
+    }
+    
+    
+    /**
+     * Tell the amount of this this alias has been used by the associated <tt>Client</tt>
+     * 
+     * @author Daniele Pantaleone
+     * @return The amount of time this alias has been used
+     **/
+    public int getNumUsed() {
+        return this.num_used;
+    }
+    
+    
+    /**
+     * Return a <tt>DateTime</tt> object representing the time when the alias has been first created
+     * 
+     * @author Daniele Pantaleone
+     * @return A <tt>DateTime</tt> object representing the time when the alias has been first created
+     **/
+    public DateTime getTimeAdd() {
+        return this.time_add;
+    }
+    
+    
+    /**
+     * Return a <tt>DateTime</tt> object representing the alias last time edit
+     * 
+     * @author Daniele Pantaleone
+     * @return A <tt>DateTime</tt> object representing the alias last time edit
+     **/
+    public DateTime getTimeEdit() {
+        return this.time_edit;
+    }
+
+    
+    /**
+     * Set the alias id
+     * 
+     * @author Daniele Pantaleone
+     * @param  id The alias id
+     **/
+    public void setId(int id) {
         this.id = id;
+    }
+
+   
+    /**
+     * Set the <tt>Client</tt> who this alias belongs to
+     * 
+     * @author Daniele Pantaleone
+     * @param  client The <tt>Client</tt> who this alias belongs to
+     **/
+    public void setClient(Client client) {
         this.client = client;
+    }
+
+    
+    /**
+     * Set the alias name
+     * 
+     * @author Daniele Pantaleone
+     * @param  name The alias name
+     **/
+    public void setName(String name) {
         this.name = name;
+    }
+    
+
+    /**
+     * Set the amount of time this alias has been used
+     * 
+     * @author Daniele Pantaleone
+     * @param  num_used The amount of time this alias has been used by the associated <tt>Client</tt>
+     **/
+    public void setNumUsed(int num_used) {
         this.num_used = num_used;
+    }
+
+    
+    /**
+     * Set the alias time add
+     * 
+     * @author Daniele Pantaleone
+     * @param  time_add A <tt>DateTime</tt> object representing the time when the alias has been first created
+     **/
+    public void setTimeAdd(DateTime time_add) {
         this.time_add = time_add;
+    }
+
+    
+    /**
+     * Set the alias last time edit
+     * 
+     * @author Daniele Pantaleone
+     * @param  time_edit A <tt>DateTime</tt> object representing the alias last time edit
+     **/
+    public void setTimeEdit(DateTime time_edit) {
         this.time_edit = time_edit;
     }
     
@@ -79,7 +211,7 @@ public class Alias {
      * @return A <tt>String</tt> representing the content of this object
      **/
     public String toString() {
-        return "[ id : " + this.id + " | client : " + this.client.id + " | name : " + this.name + " | num_used : " + this.num_used + " | time_add : " + this.time_add.toString() + " | time_edit : " + this.time_edit.toString() + " ]";   
+        return "[ id : " + this.getId() + " | client : " + this.getClient().getId() + " | name : " + this.getName() + " | num_used : " + this.getNumUsed() + " | time_add : " + this.getTimeAdd().toString() + " | time_edit : " + this.getTimeEdit().toString() + " ]";   
     }
     
 }
