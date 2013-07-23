33b0e56414a7adf6cf73f7a2b99225048fb697d9
Fenix <fenix@urbanterror.info>
2013-07-23 16:41:14 +0200

    Make the CVAR name always lowercase

--- src/com/orion/urt/Game.java
+++ src/com/orion/urt/Game.java
@@ -27,12 +27,101 @@
 
 package com.orion.urt;
 
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
 
 public class Game {
     
-    public List<String> maplist;
-    public Map<String, Cvar> cvars;
+    private List<String> maplist;
+    private Map<String, Cvar> cvarlist;
+    
+    /**
+     * Object constructor
+     * 
+     * @author Daniele Pantaleone
+     **/
+    public Game() {
+        this.maplist = new LinkedList<String>();
+        this.cvarlist = new HashMap<String, Cvar>();  
+    }
+    
+    
+    /**
+     * Return a <tt>List</tt> of maps available on the server<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     * @return The current map list
+     **/
+    public List<String> getMapList() {
+        return this.maplist;
+    }
+
+    
+    /**
+     * Set the <tt>List</tt> of maps available on the server<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     * @param  maplist The current map list
+     **/
+    public void setMapList(List<String> maplist) {
+        this.maplist = maplist;
+    }
+    
+    
+    /**
+     * Test whether a CVAR is stored in the CVAR list<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     * @param  name The CVAR name
+     * @return <tt>true</tt> if the CVAR exists, <tt>false</tt> otherwise
+     **/
+    public boolean isCvar(String name) {
+        return this.cvarlist.containsKey(name.toLowerCase());
+    }
+    
+    
+    /**
+     * Return the <tt>Cvar</tt> associated to the given name<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     * @param  name The CVAR name
+     * @return The <tt>Cvar</tt> associated to the given name or <tt>null</tt>
+     *         if the CVAR has not been retrieved and stored yet
+     **/
+    public Cvar getCvar(String name) {
+        return this.isCvar(name) ? this.cvarlist.get(name) : null;
+    }
+    
+    
+    /**
+     * Store a <tt>Cvar</tt> object in the CVAR list<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     * @param  name The CVAR name
+     * @param  cvar The <tt>Cvar</tt> object to be stored
+     **/
+    public void setCvar(String name, Cvar cvar) {
+        this.cvarlist.put(name, cvar);
+    }
+    
+    
+    /**
+     * Clear CVAR list and map list<br>
+     * NOTE: this method doesn't query the game server
+     * 
+     * @author Daniele Pantaleone
+     **/
+    public void clear() {
+        this.cvarlist.clear();
+        this.maplist.clear();
+    }
+   
     
 }
