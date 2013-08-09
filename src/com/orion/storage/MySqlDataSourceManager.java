d2229641c8a7cdb37be68cbcefbbe90f6031c3ab
Fenix <fenix@urbanterror.info>
2013-08-09 22:33:45 +0200

    Removed unnecessary craps

--- src/com/orion/storage/MySqlDataSourceManager.java
+++ src/com/orion/storage/MySqlDataSourceManager.java
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  * 
  * @author      Daniele Pantaleone
- * @version     1.0
+ * @version     1.1
  * @copyright   Daniele Pantaleone, 05 October, 2012
  * @package     com.orion.storage
  **/
@@ -35,20 +35,22 @@ import org.apache.commons.logging.Log;
 
 public class MySqlDataSourceManager implements DataSourceManager {
     
-    private Connection connection;
+    private final Log log;
+    
     private final String username;
     private final String password;
     private final String dcs;
-    private final Log log;
+    
+    private Connection connection;
     
     /**
-     * Object Constructor.
+     * Object Constructor
      * 
      * @author Daniele Pantaleone
-     * @param  username The username for DBMS connection authentication
-     * @param  password The password for DBMS connection authentication
+     * @param  username The username for storage layer authentication
+     * @param  password The password for storage layer authentication
      * @param  dcs The database connection string in JDBC notation
-     * @param  log A reference to the main lgger object
+     * @param  log A reference to the main logger object
      **/
     public MySqlDataSourceManager(String username, String password, String dcs, Log log) {
         
@@ -56,32 +58,28 @@ public class MySqlDataSourceManager implements DataSourceManager {
         this.username = username;
         this.password = password;
         this.dcs = dcs;
-        
-        // Logging storage utility configuration so the user can check if he's using a wrongs DBMS system
-        this.log.debug("Storage utility initialized [ username : " + this.username + " | password : " + this.password + " | connection : " + this.dcs + " ]");
+     
+        this.log.debug("Storage utility initialized [ username : " + this.username + 
+                                                  " | password : " + this.password + 
+                                                  " | connection : " + this.dcs + " ]");
     }
     
     
     /**
-     * Return the connection with the underline DBMS
+     * Return the connection with the storage layer
      * 
      * @author Daniele Pantaleone
      * @throws ClassNotFoundException 
-     * @throws SQLException If the connection with the DBMS fails in being executed
+     * @throws SQLException If the connection with the storage layer fails in being executed
      * @return A <tt>Connection</tt> object with the underline DBMS
      **/
     public Connection getConnection() throws ClassNotFoundException, SQLException {
         
-        // Create the connection if not already initialized.
+        // Create the connection if not already initialized
         if ((this.connection == null) || (this.connection.isClosed())) {
-            
-            long ping = System.currentTimeMillis();
             Class.forName("com.mysql.jdbc.Driver");
             this.connection = DriverManager.getConnection(this.dcs, this.username, this.password);
             this.connection.setAutoCommit(true);
-            ping = System.currentTimeMillis() - ping;
-            this.log.debug("Storage connection established [ ping : " + ping + "ms ]");
-            
         }
         
         return this.connection;
@@ -90,31 +88,26 @@ public class MySqlDataSourceManager implements DataSourceManager {
     
     
     /**
-     * Tells wheter the connection with the underline DBMS is established.
+     * Tells whether the connection with the storage layer is established<br>
      * If the connection has not been established yet, it will try to
-     * establish a new one before computing the result.
+     * establish a new one before computing the result
      * 
      * @author Daniele Pantaleone
-     * @return <tt>true</tt> if the database connection is established, 
+     * @return <tt>true</tt> if the connection with the storage layer is established, 
      *         <tt>false</tt> otherwise
      **/
     public boolean isConnection() {
         
         try {
             
-        	// Testing if the connection is already active.
-            if ((this.connection == null) || (this.connection.isClosed())) {
-                // Establishing the connection. If this fails
-            	// an Exception will be launched and by catching
-            	// it we can compute the correct result.
+        	// Testing if the connection is already active
+            if ((this.connection == null) || (this.connection.isClosed()))
             	this.connection = this.getConnection();
-            }
             
             return true;
             
         } catch (SQLException | ClassNotFoundException e) {
-            // Logging the Exception.
-            this.log.error("Unable to establish connection with database", e);
+            this.log.error("Could not connect to the storage layer", e);
             return false;
         }
         
