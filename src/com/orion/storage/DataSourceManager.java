2da5fe8ee21809b17dd9743358d4e28edd77265b
Fenix <fenix@urbanterror.info>
2013-08-09 22:27:42 +0200

    Minor JavaDoc changes

--- src/com/orion/storage/DataSourceManager.java
+++ src/com/orion/storage/DataSourceManager.java
@@ -20,7 +20,7 @@
  * THE SOFTWARE.
  * 
  * @author      Daniele Pantaleone
- * @version     1.0
+ * @version     1.1
  * @copyright   Daniele Pantaleone, 29 January, 2013
  * @package     com.orion.storage
  **/
@@ -34,23 +34,23 @@ public interface DataSourceManager {
     
     
     /**
-     * Return the connection with the underline DBMS
+     * Return the connection with the storage layer
      * 
      * @author Daniele Pantaleone
      * @throws ClassNotFoundException 
-     * @throws SQLException If the connection with the DBMS fails in being executed
+     * @throws SQLException If the connection with the storage layer fails in being executed
      * @return A <tt>Connection</tt> object with the underline DBMS
      **/
     public abstract Connection getConnection() throws ClassNotFoundException, SQLException;
    
     
     /**
-     * Tells wheter the connection with the underline DBMS is established.
+     * Tells whether the connection with the storage layer is established<br>
      * If the connection has not been established yet, it will try to
-     * establish a new one before computing the result.
+     * establish a new one before computing the result
      * 
      * @author Daniele Pantaleone
-     * @return <tt>true</tt> if the database connection is established, 
+     * @return <tt>true</tt> if the connection with the storage layer is established, 
      *         <tt>false</tt> otherwise
      **/
     public abstract boolean isConnection();
