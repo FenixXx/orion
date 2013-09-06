b64e62482556511c47a171a62b1c8b9594c3684d
Fenix <fenix@urbanterror.info>
2013-09-06 17:07:56 +0200

    Merged Splitter and MessageFormat classes into Message class

--- src/com/orion/misc/Message.java
+++ src/com/orion/misc/Message.java
@@ -21,18 +21,21 @@
  * 
  * @author      Daniele Pantaleone
  * @version     1.0
- * @copyright   Daniele Pantaleone, 16 March, 2013
- * @package     com.orion.utility
+ * @copyright   Daniele Pantaleone, 5 September, 2013
+ * @package     com.orion.misc
  **/
 
-package com.orion.utility;
+package com.orion.misc;
 
+import java.util.LinkedList;
+import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class MessageFormat {
+public class Message {
+
     
-    private static String PATTERN = "\\{\\w+\\}";
+private static String PATTERN = "\\{\\w+\\}";
     
     
     /**
@@ -77,4 +80,67 @@ public class MessageFormat {
         
     }
     
+    
+    /**
+     * Split a <tt>String</tt> into a <tt>List</tt> according to the 
+     * maxLength parameter specified. If at the split index a non-space
+     * character is found, will rollback until it finds the 1st non-space
+     * character occurrence and use that index as our split index
+     * 
+     * @author Daniele Pantaleone
+     * @param  message The <tt>String</tt> to be splitted
+     * @param  maxLength The maximum length of each splitted <tt>String</tt>
+     * @return A <tt>List</tt> containing the splitted message
+     */
+    public static List<String> split(String message, int maxLength) {
+        
+        int startIndex = 0;
+        int endIndex = maxLength - 1;
+        
+        // Building the container for our tokens
+        List<String> list = new LinkedList<String>();
+        
+        while (true) {
+            
+            if (endIndex >= message.length()) {
+      
+                // Rolling back until endIndex is not valid
+                while (endIndex  > message.length()) { 
+                    endIndex--; 
+                }
+                
+                list.add(message.substring(startIndex, endIndex));
+                break;
+                
+            } else {
+                
+                // Rolling back until we find an empty char for the split
+                while (message.charAt(endIndex - 1) != ' ') { 
+                    
+                    endIndex--;
+                    
+                    // This is to check if the string we have to
+                    // split is a CharSequence without spaces
+                    // If we do not check it here we may have 
+                    // and infinite loop which will crash the bot
+                    if (endIndex <= startIndex) {
+                        endIndex += maxLength - 1;
+                        break;
+                    }
+                    
+                }
+                
+                list.add(message.substring(startIndex, endIndex));
+                startIndex = endIndex;
+                endIndex += maxLength - 1;
+                continue;
+                
+            }
+            
+        }
+        
+        return list;
+        
+    }
+
 }
