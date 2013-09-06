ce6e2fac5d9afc3314bbd7f96e50558ca2917015
Fenix <fenix@urbanterror.info>
2013-09-06 15:12:33 +0200

    Added back RconException class

---
+++ src/com/orion/exception/RconException.java
@@ -0,0 +1,78 @@
+/**
+ * Copyright (c) 2013, Daniele Pantaleone
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ * 
+ * @author      Daniele Pantaleone
+ * @version     1.0
+ * @copyright   Daniele Pantaleone, 28 July, 2013
+ * @package     com.orion.exception
+ **/
+
+package com.orion.exception;
+
+public class RconException extends Exception {
+
+    private static final long serialVersionUID = 1L;
+    
+    
+    /**
+     * Build a <tt>RconException</tt> object
+     * 
+     * @author Daniele Pantaleone
+     **/
+    public RconException() {
+        super();
+    }
+    
+    
+    /**
+     * Build a <tt>RconException</tt> object
+     * 
+     * @author Daniele Pantaleone
+     * @param  message The <tt>Exception</tt> message
+     * @param  cause A <tt>Throwable</tt> cause
+     **/
+    public RconException(String message, Throwable cause) {
+        super(message, cause);
+    }
+    
+    
+    /**
+     * Build a <tt>RconException</tt> object
+     * 
+     * @author Daniele Pantaleone
+     * @param  message The <tt>Exception</tt> message
+     **/
+    public RconException(String message) {
+        super(message);
+    }
+    
+    
+    /**
+     * Build a <tt>RconException</tt> object
+     * 
+     * @author Daniele Pantaleone
+     * @param  cause A <tt>Throwable</tt> cause
+     **/
+    public RconException(Throwable cause) {
+        super(cause);
+    }
+    
+}
